3aef9c43ba45e0374dbb9805d158acc06e062f5e
VoidElle <lucadelc@gmail.com>
2024-02-10 20:54:52 +0100

    fix: Fixed a big bug on the player's scoring system

--- src/events/game/game_score_event.ts
+++ src/events/game/game_score_event.ts
@@ -2,6 +2,8 @@ import { EventBaseInterface } from "../../interfaces/event_base_interface";
 import { LoggerUtils, LogTypes } from "../../utils/loggerUtils";
 import {Server, Socket} from "socket.io";
 import {Events} from "../../utils/events";
+import {RemoteSocket} from "socket.io/dist/broadcast-operator";
+import {DefaultEventsMap} from "socket.io/dist/typed-events";
 
 export class GameScoreEvent implements EventBaseInterface {
 
@@ -19,9 +21,21 @@ export class GameScoreEvent implements EventBaseInterface {
 
         LoggerUtils.log(LogTypes.INFO, `Game score event triggered from socket ${this.socket.id}`);
 
+        // Get the list of sockets connected in the lobby
+        const socketsInRoom: RemoteSocket<DefaultEventsMap, any>[] = await this.io.in(this.lobbyId).fetchSockets();
+
+        // Gather the victim's socket id
+        let victimId: String;
+        socketsInRoom.map((socket: RemoteSocket<DefaultEventsMap, any>): void => {
+            if (socket.id != this.socket.id) {
+                victimId = socket.id;
+            }
+        });
+
         // Generate the response in a json format
         const jsonResponse: JSON = <JSON><any>{
-            "socketId": this.socket.id,
+            "attackerId": this.socket.id,
+            "victimId": victimId,
         }
 
         this.io.to(this.lobbyId).emit(Events.GAME_SCORE_RESPONSE_SUCCESS, jsonResponse);

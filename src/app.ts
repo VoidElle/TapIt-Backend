a4982be3f8078c642433da2c4ab4ec7442e9d703
luca_delcorona <luca.delcorona@avvale.com>
2024-10-02 23:18:24 +0200

    chore: Improved events registration and handling

--- src/app.ts
+++ src/app.ts
@@ -22,9 +22,29 @@ serverSession.io.on(CustomEvents.CONNECTION, socket => {
 
 function eventHandling(socket: Socket) {
 
-    // Core events
-    socket.on(CustomEvents.DISCONNECT, () => new DisconnectionEvent(new EventModel(undefined, socket)).manageEvent());
+    const eventHandlers = {
 
+        // Core events
+        [CustomEvents.DISCONNECT]: () => new DisconnectionEvent(new EventModel(undefined, socket)).manageEvent(),
+
+        /*[Events.JOIN_LOBBY_REQUEST]: (lobbyId: string) => new JoinLobbyEvent(lobbyId, socket, io),
+        [Events.QUIT_LOBBY_REQUEST]: (lobbyId: string) => new QuitLobbyEvent(lobbyId, socket, io),
+        [Events.PLAYER_CHANGE_READY_STATUS]: (lobbyId: string) => new PlayerChangeReadyStatusEvent(lobbyId, socket, io),
+        [Events.START_LOBBY_REQUEST]: (lobbyId: string) => new StartLobbyEvent(lobbyId, socket, io)*/
+    };
+
+    // Loop for listening all the events
+    Object.keys(eventHandlers).forEach((event) => {
+        socket.on(event, (...args) => {
+
+            const handler = eventHandlers[event](...args);
+            if (!handler || !handler.manageEvent) {
+                return;
+            }
+
+            handler.manageEvent();
+        });
+    });
 
 }
 
